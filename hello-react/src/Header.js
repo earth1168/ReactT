15a714d33089b42004c2458545a2b61c57d5eaac
Earth <pimanus1234@gmail.com>
2019-01-04 10:00:35 +0700

    Update header.js State word-Count

--- hello-react/src/Header.js
+++ hello-react/src/Header.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 class Header extends Component{
+  constructor(){
+    super();
+    this.state={
+        name:"Unlike",
+        count:0
+    };
+  }
     render(){ 
+      setTimeout(()=>{
+        this.setState({name:"Like"});
+      },2000);
+      setInterval(()=>{
+        this.setState({count:this.state.count+1});
+      },100);
       return(
         <div>
             <h1>React Tutorial</h1>
+            <h2>{this.state.name}</h2>
+            <h2>{this.state.count}</h2>
        </div>
       );
     }  
